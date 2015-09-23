1b2651c0558c29eab5620617b4b65edc8ed94979
elex <elextraza@gmail.com>
2015-09-23 22:10:12 +0300

    Менюшка

--- ProductAdmin/menuSnipet.php
+++ ProductAdmin/menuSnipet.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: elex
- * Date: 23.09.15
- * Time: 20:15
- */
+<div class="w-clearfix menu">
+    <div class="menu-item active"><a class="menu-item-text active" href="#">Главная</a></div>
+    <div class="menu-item menu-oplata"><a class="menu-item-text" href="#">Оплата</a></div>
+    <div class="menu-item menu-dostavka"><a class="menu-item-text" href="#">Доставка</a></div>
+    <div class="menu-item"><a class="menu-item-text" href="#">контакты</a></div>
+    <div class="menu-item menu-ort"><a class="menu-item-text" href="#">обратная связь</a></div>
+</div>
